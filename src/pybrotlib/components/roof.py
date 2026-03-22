d2ba602cc677d66b4ceac623979c8fec0f58f67e
Tim-Oliver Husser <thusser@uni-goettingen.de>
2026-03-22 13:58:01 +0200

    roof state

--- src/pybrotlib/components/roof.py
+++ src/pybrotlib/components/roof.py
@@ -16,25 +16,24 @@ class RoofStatus(Enum):
 class BROTRoof(BROTBase):
     @property
     def in_motion(self) -> bool:
-        return (self._telemetry.AUXILIARY.DOME.REALPOS == 0.5) or (
-            self._telemetry.AUXILIARY.DOME.MOTION_STATE == 1.0
-        )
+        return self._telemetry.AUXILIARY.DOME.MOTION_STATE == 1.0
 
     @property
     def status(self) -> RoofStatus:
-        match self._telemetry.AUXILIARY.DOME.READY_STATE:
-            case 0.0:
-                return RoofStatus.CLOSED
-            case 0.3:
-                return RoofStatus.CLOSING
-            case 0.5:
+        if self._telemetry.AUXILIARY.DOME.REALPOS == 0.0:
+            return RoofStatus.CLOSED
+        elif self._telemetry.AUXILIARY.DOME.REALPOS == 1.0:
+            return RoofStatus.OPEN
+        elif self._telemetry.AUXILIARY.DOME.REALPOS == -1.0:
+            return RoofStatus.ERROR
+        else:
+            if self._telemetry.AUXILIARY.DOME.MOTION_STATE == 0.0:
                 return RoofStatus.STOPPED
-            case 0.7:
-                return RoofStatus.OPENING
-            case -1.0:
-                return RoofStatus.ERROR
-            case _:
-                return RoofStatus.UNKNOWN
+            else:
+                if self._telemetry.AUXILIARY.DOME.TARGETPOS == 0.0:
+                    return RoofStatus.CLOSING
+                else:
+                    return RoofStatus.OPENING
 
     @property
     def error_state(self) -> bool:
